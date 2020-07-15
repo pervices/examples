9abcbf558f2f569793b69ea95abe1095a7e23cfe
Vikram Venkataramanan <vikram.v@pervices.com>
2020-07-15 16:18:36 +0000

    added error message for ref source

--- source/fm_radio.cpp
+++ source/fm_radio.cpp
@@ -1,6 +1,12 @@
 #include <boost/format.hpp>
 #include <boost/program_options.hpp>
 #include <chrono>
+#include <csignal>
+/* #include <gnuradio/audio/sink.h> */
+/* #include <gnuradio/blocks/complex_to_float.h> */
+#include <gnuradio/top_block.h>
+/* #include <gnuradio/uhd/usrp_sink.h> */
+/* #include <gnuradio/uhd/usrp_source.h> */
 #include <thread>
 #include <uhd/exception.hpp>
 #include <uhd/types/tune_request.hpp>
@@ -8,44 +14,56 @@
 #include <uhd/utils/safe_main.hpp>
 #include <uhd/utils/thread.hpp>
 
-const int msec_per_sec = 1e3;
+//--------------------------------------------------------------------------------------------------
+//-- Constants
+//--------------------------------------------------------------------------------------------------
 const int audio_samp_rate = 48e3;
 
 namespace po = boost::program_options;
 
-void print_usage()
+//--------------------------------------------------------------------------------------------------
+//-- Signal Handlers
+//--------------------------------------------------------------------------------------------------
+static bool stop_signal_called = false;
+void sig_int_handler(int) { stop_signal_called = true; }
+
+//--------------------------------------------------------------------------------------------------
+//-- Program
+//--------------------------------------------------------------------------------------------------
+void print_usage(po::options_description desc)
 {
     std::cout << "Usage: fm_radio [OPTIONS] ..." << std::endl
               << "Receives frequency modulated signals from the specified station." << std::endl
               << std::endl
-              << "Options:" << std::endl // TODO
-              << "\t -a (device args)" << std::endl
-              << "\t -f (frequency in Hz)" << std::endl
-              << "\t -r (sample rate in Hz)" << std::endl
-              << "\t -g (gain)" << std::endl
-              << "\t -n (number of samples to receive)" << std::endl
-              << "\t -h (print this help message)" << std::endl
+              << desc << std::endl
               << std::endl
               << "Examples:" << std::endl
-              << "\t fm_radio --help" << std::endl // TODO
+              << "    fm_radio --help" << std::endl // TODO
               << std::endl;
 }
 
-int set_program_args(int argc, char *argv[])
+int UHD_SAFE_MAIN(int argc, char *argv[])
 {
-    std::string args, ant, ref_src;
-    double rate, freq, gain, setup_time;
+    uhd::set_thread_priority_safe();
+
+    //------------------------------------------------------------------
+    //-- Set program args
+    //------------------------------------------------------------------
+    std::string device_addr, ant, ref_src;
+    double desired_sample_rate, center_frequency, desired_gain, setup_time;
     size_t channel;
 
     po::options_description desc("Allowed options");
     // clang-format off
     desc.add_options()
       ("help", "help message")
-      ("args", po::value<std::string>(&args)->default_value(""), "multi uhd device address args")
+      ("device", po::value<std::string>(&device_addr)->default_value(""), "multi uhd device address args")
       ("channel", po::value<size_t>(&channel)->default_value(0), "which channel to use")
-      ("freq", po::value<double>(&freq)->default_value(0.0), "RF center frequency in Hz")
-      ("gain", po::value<double>(&gain)->default_value(1.0), "gain for the RF chain")
-      ("rate", po::value<double>(&rate)->default_value(1e6), "rate of incoming samples")
+      ("center_freq", po::value<double>(&center_frequency)->default_value(99.9e6),
+                      "RF center frequency in Hz")
+      ("gain", po::value<double>(&desired_gain)->default_value(1.0), "gain for the RF chain")
+      ("samp_rate", po::value<double>(&desired_sample_rate)->default_value(1e6),
+                    "rate of incoming samples")
       ("ref_src", po::value<std::string>(&ref_src)->default_value("internal"),
                   "reference source (internal, external, mimo)")
       ("setup", po::value<double>(&setup_time)->default_value(1.0), "seconds of setup time")
@@ -57,53 +75,87 @@ int set_program_args(int argc, char *argv[])
 
     std::cout << std::endl;
 
-    if (vm.count("help") || argc <= 1) {
-        print_usage();
+    if (vm.count("help") or argc <= 1) {
+        print_usage(desc);
         return ~0;
     }
 
-    // === Instantiate crimson device ===
-    std::cout << boost::format("Instantiating the usrp crimson device with: %s...") % args
+    //------------------------------------------------------------------
+    //-- Instantiate Crimson Device
+    //------------------------------------------------------------------
+    if (not((ref_src.compare("internal")) == 0 or (ref_src.compare("external")) == 0 or
+            (ref_src.compare("mimo")) == 0)) {
+        std::cerr << "Invalid reference source. Reference source should be one of (internal, "
+                     "external, mimo)"
+                  << std::endl;
+        return 0;
+    }
+    std::cout << boost::format("Instantiating the usrp crimson device with: %s...") % device_addr
               << std::endl;
-    uhd::usrp::multi_usrp::sptr crimson = uhd::usrp::multi_usrp::make(args);
+    uhd::usrp::multi_usrp::sptr crimson = uhd::usrp::multi_usrp::make(device_addr);
     crimson->set_clock_source(ref_src); // lock mboard clocks
 
-    // === Set the sample rate ===
-    double actual_rate;
-    if (rate <= 0.0) {
-        std::cerr << boost::format("%s is not a valid sample rate.")
-                  << "The sample rate needs to be >= 0.0"
+    //------------------------------------------------------------------
+    //-- Set the Sample Rate
+    //------------------------------------------------------------------
+    double actual_sample_rate;
+    if (desired_sample_rate <= 0.0) {
+        std::cerr << boost::format("%s is not a valid sample rate.") % desired_sample_rate
+                  << "The sample rate needs to be a positive float"
                   << "Please specify a valid sample rate." << std::endl;
         return ~0;
     }
-    std::cout << boost::format("Setting RX Rate: %f Msps...") % (rate / 1e6) << std::endl;
-    crimson->set_rx_rate(rate, channel);
-    actual_rate = crimson->get_rx_rate(channel) / 1e6;
-    std::cout << boost::format("Actual RX Rate: %f Msps...") % actual_rate << std::endl;
+    std::cout << boost::format("Setting RX Rate: %f Msps...") % (desired_sample_rate / 1e6)
+              << std::endl;
+    crimson->set_rx_rate(desired_sample_rate, channel);
+    actual_sample_rate = crimson->get_rx_rate(channel) / 1e6;
+    std::cout << boost::format("Actual RX Rate: %f Msps...") % actual_sample_rate << std::endl;
 
-    // === Set the rf gain ===
+    //------------------------------------------------------------------
+    //-- Set the RF Gain
+    //------------------------------------------------------------------
     double actual_gain;
     if (vm.count("gain")) {
-        std::cout << boost::format("Setting RX Gain: %f dB...") % gain << std::endl;
-        crimson->set_rx_gain(gain, channel);
+        std::cout << boost::format("Setting RX Gain: %f dB...") % desired_gain << std::endl;
+        crimson->set_rx_gain(desired_gain, channel);
         actual_gain = crimson->get_rx_gain(channel);
         std::cout << boost::format("Actual RX Gain: %f dB...") % actual_gain << std::endl;
     }
 
-    // sleep a bit while the slave locks its time to the master
-    std::this_thread::sleep_for(std::chrono::milliseconds(int64_t(setup_time * msec_per_sec)));
+    //------------------------------------------------------------------
+    //-- Sleep a bit while the slave locks its time to the master
+    //------------------------------------------------------------------
+    std::this_thread::sleep_for(std::chrono::seconds(int64_t(setup_time)));
 
-    return 0;
-}
+    //  ================================================================================
+    /* gr::top_block_sptr tb = gr::make_top_block("program_name"); */
 
-int UHD_SAFE_MAIN(int argc, char *argv[])
-{
-    uhd::set_thread_priority_safe();
+    /* gr::uhd::usrp_source::sptr usrp_source = */
+    /* gr::uhd::usrp_source::make(device_addr, uhd::stream_args_t("fc32")); */
+    /* usrp_source->set_samp_rate(rate); */
+    /* usrp_source->set_center_frequency(freq); */
 
-    if (set_program_args(argc, argv) != 0) {
-        return ~0;
+    /* gr::uhd::usrp_sink::sptr usrp_sink = gr::uhd::usrp_sink::make(device_addr,
+     * uhd::stream_args_t("fc32")); */
+    /* usrp_sink->set_samp_rate(rate); */
+    /* usrp_sink->set_center_frequency(freq); */
+    /* tb->connect(usrp_source, 0, usrp_sink, 0); */
+    // ================================================================================
+
+    //------------------------------------------------------------------
+    //-- poll the exit signal while running
+    //------------------------------------------------------------------
+    /* std::cout << "starting flow graph" << std::endl; */
+    /* tb->start(); */
+
+    std::signal(SIGINT, &sig_int_handler);
+    std::cout << "Press ctrl + c to exit" << std::endl;
+    while (not stop_signal_called) {
+        boost::this_thread::sleep(boost::posix_time::milliseconds(100));
     }
 
-    std::cout << std::endl << "Done!" << std::endl;
+    std::cout << std::endl << "done!" << std::endl;
     return EXIT_SUCCESS;
 }
+
+//--------------------------------------------------------------------------------------------------
