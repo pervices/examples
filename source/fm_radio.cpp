c3a282b32ec2c6b2c31861df24f831e2e7cb76bc
Vikram Venkataramanan <vikram.v@pervices.com>
2020-07-27 20:49:22 +0000

    code compiles!

--- source/fm_radio.cpp
+++ source/fm_radio.cpp
@@ -1,5 +1,6 @@
 #define _USE_MATH_DEFINES
 
+#include <algorithm>
 #include <boost/format.hpp>
 #include <boost/program_options.hpp>
 #include <chrono>
@@ -8,10 +9,12 @@
 #include <gnuradio/audio/sink.h>
 #include <gnuradio/blocks/complex_to_float.h>
 #include <gnuradio/filter/fir_filter_fff.h>
+#include <gnuradio/filter/pm_remez.h>
 #include <gnuradio/filter/rational_resampler_base_ccc.h>
 #include <gnuradio/top_block.h>
 #include <gnuradio/uhd/usrp_sink.h>
 #include <gnuradio/uhd/usrp_source.h>
+#include <iostream>
 #include <math.h>
 #include <thread>
 #include <uhd/exception.hpp>
@@ -19,17 +22,6 @@
 #include <uhd/usrp/multi_usrp.hpp>
 #include <uhd/utils/safe_main.hpp>
 #include <uhd/utils/thread.hpp>
-
-#define K(CHANNEL_RATE, DEVIATION) (CHANNEL_RATE / 2 * M_PI * DEVIATION)
-/* #define STOPBAND_ATTEN_TO_DEV(ATTEN_DB)  (std::pow(10, -ATTEN_DB / 20)) */
-/* #define PASSBAND_ATTEN_TO_DEV(RIPPLE_DB) (10 * *(RIPPLE_DB / 20) - 1) / (10 * *(RIPPLE_DB / 20) +
- * 1) */
-
-#define debug_print(fmt, ...)                                                                      \
-    do {                                                                                           \
-        if (DEBUG)                                                                                 \
-            fprintf(stderr, fmt, __VA_ARGS__);                                                     \
-    } while (0)
 
 /**
  * copy files over with:
@@ -71,8 +63,188 @@ static bool stop_signal_called = false;
 void sig_int_handler(int) { stop_signal_called = true; }
 
 //--------------------------------------------------------------------------------------------------
+//-- Helper Functions
+//--------------------------------------------------------------------------------------------------
+
+std::vector<float> convert_vector_doubles_to_floats(std::vector<double> vec_doubles)
+{
+    std::vector<float> vec_floats;
+    for (int i = 0; i < vec_doubles.size(); i++) {
+        vec_floats.push_back(vec_doubles[i]);
+    }
+
+    return vec_floats;
+}
+
+double vector_max(std::vector<double> &arr)
+{
+    double max = INT_MIN;
+    for (auto val : arr) {
+        if (max < val)
+            max = val;
+    }
+    return max;
+}
+
+float lporder(float freq1, float freq2, float delta_p, float delta_s)
+{
+    /**
+     * FIR lowpass filter length estimator.  freq1 and freq2 are
+     * normalized to the sampling frequency.  delta_p is the passband
+     * deviation (ripple), delta_s is the stopband deviation (ripple).
+     * Note, this works for high pass filters too (freq1 > freq2), but
+     * doesn't work well if the transition is near f == 0 or f == fs/2
+     * From Herrmann et al (1973), Practical design rules for optimum
+     * finite impulse response filters.
+     *
+     * Bell System Technical J., 52, 769-99
+     **/
+
+    float df = std::abs(freq2 - freq1);
+    float ddp = std::log10(delta_p);
+    float dds = std::log10(delta_s);
+
+    float a1 = 5.309e-3;
+    float a2 = 7.114e-2;
+    float a3 = -4.761e-1;
+    float a4 = -2.66e-3;
+    float a5 = -5.941e-1;
+    float a6 = -4.278e-1;
+
+    float b1 = 11.01217;
+    float b2 = 0.5124401;
+
+    float t1 = a1 * ddp * ddp;
+    float t2 = a2 * ddp;
+    float t3 = a4 * ddp * ddp;
+    float t4 = a5 * ddp;
+
+    float dinf = ((t1 + t2 + a3) * dds) + (t3 + t4 + a6);
+    float ff = b1 + b2 * (ddp - dds);
+    float n = dinf / df - ff * df + 1;
+
+    return n;
+}
+
+std::vector<double> remezord(std::vector<double> fcuts, std::vector<double> mags,
+                             std::vector<double> devs, int nextra_taps, int fsamp = 2)
+{
+
+    float nbands = mags.size();
+
+    /**
+     * fsamp defaults to 2 Hz, implying a Nyquist frequency of 1 Hz.
+     * You can therefore specify band edges scaled to a particular applications.
+     * sampling frequency.
+     */
+    for (int i = 0; i < fcuts.size(); i++) {
+        fcuts[i] = fcuts[i] / fsamp;
+    }
+
+    if (mags.size() != devs.size()) {
+        std::cerr << "Length of mags and devs must be equal";
+    }
+
+    if (fcuts.size() != 2 * (nbands - 1)) {
+        std::cerr << "Length of f must be 2 * len (mags) - 2";
+    }
+
+    for (int i = 1; i < mags.size(); i++) {
+        devs[i] = devs[i] / mags[i];
+    }
+
+    std::vector<double> f1, f2;
+    for (int i = 0; i < fcuts.size(); i++) {
+        if (i % 2 == 0) {
+            f1.push_back(fcuts[i]);
+        } else {
+            f2.push_back(fcuts[i]);
+        }
+    }
+
+    int n = 0;
+    int min_delta = 2;
+
+    for (int i = 0; i < f1.size(); i++) {
+        if (f2[i] - f1[i] < min_delta) {
+            n = i;
+            min_delta = f2[i] - f1[i];
+        }
+    }
+
+    float l, l1, l2;
+    if (nbands = 2) {
+        l = lporder(f1[n], f2[n], devs[0], devs[1]);
+    } else {
+        l = 0;
+        for (int i = 1; i < nbands - 1; i++) {
+            l1 = lporder(f1[i - 1], f2[i - 1], devs[i], devs[i - 1]);
+            l2 = lporder(f1[i], f2[i], devs[i], devs[i + 1]);
+            l = std::max(l, l1);
+            l = std::max(l, l2);
+        }
+    }
+
+    n = (int) (ceil(l) - 1);
+
+    std::vector<double> fo;
+    fo.push_back(0.0);
+    for (int i = 0; i < fcuts.size(); i++) {
+        fo.push_back(fcuts[i]);
+    }
+    fo.push_back(1.0);
+    for (int i = 0; i < fo.size(); i++) {
+        fo[i] *= 2;
+    }
+
+    std::vector<double> ao;
+    float a;
+    for (int i = 0; i < mags.size(); i++) {
+        a = mags[i];
+        ao.push_back(a);
+        ao.push_back(a);
+    }
+
+    std::vector<double> wts;
+    double max_deviations = vector_max(devs);
+    for (int i = 0; i < devs.size(); i++) {
+        wts[i] = max_deviations / devs[i];
+    }
+
+    std::vector<double> remez_out = gr::filter::pm_remez(n + nextra_taps, fo, ao, wts);
+    return remez_out;
+}
+
+std::vector<float> low_pass(float gain, float samp_rate, float passband_end, float stopband_start,
+                            float passband_ripple_db = 0.1, float stopband_atten_db = 60,
+                            int nextra_taps = 2)
+{
+    std::vector<double> frequency_band_edges, magnitudes, max_deviations;
+
+    float passband_dev = (std::pow(10, (passband_ripple_db / 20)) - 1) /
+                         (std::pow(10, (passband_ripple_db / 20) + 1));
+    float stopband_dev = std::pow(10, -stopband_atten_db / 20);
+
+    frequency_band_edges.push_back(passband_end);
+    frequency_band_edges.push_back(stopband_start);
+
+    magnitudes.push_back(gain);
+    magnitudes.push_back(0);
+
+    max_deviations.push_back(stopband_dev);
+    max_deviations.push_back(passband_dev);
+
+    std::vector<double> remez_out =
+        remezord(frequency_band_edges, magnitudes, max_deviations, nextra_taps, samp_rate);
+    std::vector<float> taps = convert_vector_doubles_to_floats(remez_out);
+
+    return taps;
+}
+
+//--------------------------------------------------------------------------------------------------
 //-- Program
 //--------------------------------------------------------------------------------------------------
+
 void print_usage(po::options_description desc)
 {
     std::cout << "Usage: fm_radio [OPTIONS] ..." << std::endl
@@ -210,28 +382,18 @@ int UHD_SAFE_MAIN(int argc, char *argv[])
     tb->connect(usrp_source, 0, resampler, 0);
 
     // Demodulate quadrature
-    float deviation = 150e3;
+    float deviation = 15e3;
     float k = user_args->channel / (2 * M_PI * deviation);
     gr::analog::quadrature_demod_cf::sptr quad_demod = gr::analog::quadrature_demod_cf::make(k);
     tb->connect(resampler, 0, quad_demod, 0);
 
-    // TODO: low_pass
-    std::vector<float> audio_taps;  // TODO: WTF is a tap?
-    float audio_pass = 15000;
-    float audio_stop = 16000;
-    float passband_ripple = 0.1;
-    float stopband_attenuation = 60;
-    float stopband_dev = std::pow(10, -stopband_attenuation / 20);
-    float passband_dev =
-        (std::pow(10, (passband_ripple / 20)) - 1) / (pow(10, (passband_ripple / 20) + 1));
-
-    /* std::vector<double> garbage; */
-    /* garbage.push_back(1.0, 2.0); */
-    /* std::vector<double> pm_remez_filter = */
-    /*     gr::filter::pm_remez(1, garbage, garbage, garbage, "bandpass", 16); */
+    // Low pass filter to obtain taps
+    float audio_pass = 15e3;
+    float audio_stop = 16e3;
+    std::vector<float> audio_taps =
+        low_pass(user_args->gain, user_args->sample_rate, audio_pass, audio_stop);
 
     // fir_filter_fff
-    audio_taps.push_back(1.0);
     gr::filter::fir_filter_fff::sptr fir_filter = gr::filter::fir_filter_fff::make(1, audio_taps);
     tb->connect(quad_demod, 0, fir_filter, 0);
 
@@ -251,7 +413,6 @@ int UHD_SAFE_MAIN(int argc, char *argv[])
     while (not stop_signal_called) {
         boost::this_thread::sleep(boost::posix_time::milliseconds(100));
     }
-
     tb->stop();
     tb->wait();
 
