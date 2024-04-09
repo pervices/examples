a9244b1b6b389dc550e273d0772471f2747ae867
John Polak <john.p@pervices.com>
2024-04-09 18:00:32 +0000

    SDR2SDR: adding 2 channel scripts

---
+++ sdr2sdr/1gsps-cyan-to-cyan-midband-ab.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+rxaddr=192.168.10.2
+txaddr=192.168.11.2
+#rxchan=0,1,2,3
+#txchan=0,1,2,3
+rxchan=0,1
+txchan=0,1
+# chosen for ~-10dBm in
+rxgain=40
+txgain=15
+rxfreq=2e9
+txfreq=2e9
+bandwidth=1e9
+
+/usr/lib/uhd/examples/rxtx_inter_device_stream --rx_args addr=$rxaddr --tx_args addr=$txaddr --rate $bandwidth --rx_channels $rxchan --tx_channels $txchan --rx_gain $rxgain --tx_gain $txgain --rx_freq $rxfreq --tx_freq $txfreq --ref external
+
---
+++ sdr2sdr/crimson-to-crimson-highband-ab.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+rxaddr=192.168.10.12
+txaddr=192.168.10.13
+rxchan=0,1
+txchan=0,1
+# chosen for ~-10dBm input
+rxgain=21
+txgain=20
+rxfreq=2000e6
+txfreq=2000e6
+bandwidth=162.5e6
+
+/usr/lib/uhd/examples/rxtx_inter_device_stream --rx_args addr=$rxaddr --tx_args addr=$txaddr --rate $bandwidth --rx_channels $rxchan --tx_channels $txchan --rx_gain $rxgain --tx_gain $txgain --rx_freq $rxfreq --tx_freq $txfreq --ref external
+
