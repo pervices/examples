2f0d72e4a7ba04ff9a03ddde7144c3670869dcff
Doug Wilson <doug.w@pervices.com>
2026-08-17 18:05:13 -0400

    MIN_SEND: implementing send

--- minimal_uhdless_demo/send_main.cpp
+++ minimal_uhdless_demo/send_main.cpp
@@ -1,4 +1,6 @@
 #include <iostream>
+#include <complex>
+
 #include "pv_iface.hpp"
 #include "clock_sync.hpp"
 #include "super_send_packet_handler_mmsg.hpp"
@@ -26,7 +28,7 @@ int main() {
     // TODO: set gain and frequency
 
     std::vector<size_t> channels = {0};
-    size_t max_samples_per_packet = /*8912 is the maximum bytes samples can take up. sc16 are 4 bytes each*/ 8912 / 4;
+    int64_t max_samples_per_packet = /*8912 is the maximum bytes samples can take up. sc16 are 4 bytes each*/ 8912 / 4;
     int64_t buffer_size = (int64_t) management_iface.get_double("system/get_max_buffer_level");
     std::vector<std::string> dst_ips = { "10.10.10.2" };
     std::vector<int> dst_ports = { 42836 };
@@ -54,13 +56,45 @@ int main() {
             clock_sync_manager
         );
 
-    /* NOTE:
-     * certain hardware requires packets to be a multiple of a certain length
-     * You can get that from system/nsamps_multiple_tx. I will hardcode it for now
-    */
-
-
     
+    management_iface.set_double("tx/a/dsp/rate", 10e6);
+    double actual_sample_rate = management_iface.get_double("tx/a/dsp/rate");
+    std::cout << "Actual sample rate: " << actual_sample_rate << std::endl;
+    send_manager.set_samp_rate(actual_sample_rate);
+
+    // Vector of samples long enough to send up to 10 packets of samples at a time
+    std::vector<std::complex<short>> samples((size_t) max_samples_per_packet * 10, 0);
+
+    // Pointers to the start of the sample buffer
+    // The streamer tacks an arrays of pointers to the buffers of samples for each channel
+    std::vector<const void* > sample_ptrs(1, samples.data());
+
+    // Wait for clock (predicting the time of arrival of packets) to complete
+    clock_sync_manager->wait_for_sync();
+
+    time_spec_t send_start_time = clock_sync_manager->get_device_time() + 10.0;
+
+    tx_metadata_t md;
+    md.start_of_burst = true;
+    md.end_of_burst   = false;
+    md.has_time_spec  = true;
+    md.time_spec = send_start_time;
+
+    std::cout << "Starting sending samples\n";
+
+    size_t samples_sent = 0;
+    // Send 10 seconds of samples
+    size_t samples_to_send = (size_t) actual_sample_rate * 10;
+
+    while(samples_sent < samples_to_send) {
+        size_t samples_this_send = std::min(samples.size(), samples_to_send - samples_sent);
+        samples_sent += send_manager.send(sample_ptrs, samples_this_send, md, 5);
+
+        md.start_of_burst = false;
+        md.has_time_spec = false;
+    }
+
+    std::cout << "Sent " << samples_sent << " samples\n";
 
     return 0;
 }
