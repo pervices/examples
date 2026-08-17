2951ab17ebc917cc2fecb6ce053587a57e64257b
Doug Wilson <doug.w@pervices.com>
2026-08-17 15:59:38 -0400

    MIN_SEND: connect to management port

--- minimal_uhdless_demo/send_main.cpp
+++ minimal_uhdless_demo/send_main.cpp
@@ -1,6 +1,12 @@
 #include <iostream>
+#include "pv_iface.hpp"
 
 int main() {
-    std::cout << "Hellow world" << std::endl;
+    std::cout << "Starting" << std::endl;
+
+    // Create an instance of the class to manage communication with the management port
+    uhd::pv_iface management_iface = uhd::pv_iface("192.168.10.2", 42799);
+
+    std::cout << "Created connection to management port" << std::endl;
     return 0;
 }
